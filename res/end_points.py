3d35821bd5645f5f7d8d10084d9266a3a8f262e8
Yash Zanwar <yszanwar@gmail.com>
2022-11-19 18:01:22 +0530

    added TickFlow

--- res/end_points.py
+++ res/end_points.py
@@ -1,55 +1,60 @@
 import warnings
 
 class EndPoints():
+
+    def __init__(self):
+        #GraphQL pagination inital values
+        self.step = 1000
+        self.page = 0
+
+        #Graph_QL_Endpoints
+        self.mainnet_gql = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
+        self.polygon_gql = "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon"
+        self.optimism_gql = "https://api.thegraph.com/subgraphs/name/ianlapham/optimism-post-regenesis"
+        self.arbitrum_gql = "https://api.thegraph.com/subgraphs/name/ianlapham/arbitrum-minimal"
+        self.celo_gql = "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-celo"
+        self.goerli_gql = "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-gorli"
+
+        #RPC Endpoints
+        self.mainnet_rpc = "https://rpc.flashbots.net/"
+        self.polygon_rpc = "https://polygon-rpc.com"
+        self.optimism_rpc = "https://mainnet.optimism.io/"
+        self.arbitrum_rpc = "https://rpc.ankr.com/arbitrum"
+        self.celo_rpc = "https://forno.celo.org"
+        self.goerli_rpc = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
+        pass
     
-    #GraphQL pagination
-    step = 1000
-    page = 0
 
-    mainnet_gql = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
-    polygon_gql = "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-polygon"
-    optimism_gql = "https://api.thegraph.com/subgraphs/name/ianlapham/optimism-post-regenesis"
-    arbitrum_gql = "https://api.thegraph.com/subgraphs/name/ianlapham/arbitrum-minimal"
-    celo_gql = "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-celo"
-    goerli_gql = "https://api.thegraph.com/subgraphs/name/ianlapham/uniswap-v3-gorli"
-
-    mainnet_rpc = "https://rpc.flashbots.net/"
-    polygon_rpc = "https://polygon-rpc.com"
-    optimism_rpc = "https://mainnet.optimism.io/"
-    arbitrum_rpc = "https://rpc.ankr.com/arbitrum"
-    celo_rpc = "https://forno.celo.org"
-    goerli_rpc = "https://goerli.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"
-
-    def getGraphQLEndpoint(chain):
+    def getGraphQLEndpoint(self, chain):
         warnings.warn("Warning: Using thegraph.com API endpoints.")
         if(chain=="mainnet"):
-            return EndPoints.mainnet_gql
+            return [self.mainnet_gql, self.step, self.page]
         if(chain=="polygon"):
-            return EndPoints.mainnet_gql
+            return [self.polygon_gql, self.step, self.page]
         if(chain=="optimism"):
-            return EndPoints.optimism_gql
+            return [self.optimism_gql, self.step, self.page]
         if(chain=="arbitrum"):
-            return EndPoints.arbitrum_gql
+            return [self.arbitrum_gql, self.step, self.page]
         if(chain=="celo"):
-            return EndPoints.celo_gql
+            return [self.celo_gql, self.step, self.page]
         if(chain=="goerli"):
-            return EndPoints.goerli_gql
+            return [self.goerli_gql, self.step, self.page]
         print("No Chain Found")
         return 0
     
-    def getRPCEndpoints(chain):
+    def getRPCEndpoints(self, chain):
         warnings.warn("Warning: Using RPC API endpoints.")
         if(chain=="mainnet"):
-            return EndPoints.mainnet_rpc
+            return self.mainnet_rpc
         if(chain=="polygon"):
-            return EndPoints.mainnet_rpc
+            return self.polygon_rpc
         if(chain=="optimism"):
-            return EndPoints.optimism_rpc
+            return self.optimism_rpc
         if(chain=="arbitrum"):
-            return EndPoints.arbitrum_rpc
+            return self.arbitrum_rpc
         if(chain=="celo"):
-            return EndPoints.celo_rpc
+            return self.celo_rpc
         if(chain=="goerli"):
-            return EndPoints.goerli_rpc
+            return self.goerli_rpc
         print("No Chain Found")
         return 0
--- src/PoolState.py
+++ src/PoolState.py
@@ -1,8 +1,124 @@
 import pandas as pd
+import requests
 
 class PoolState():
 
-    def __init__(self, address):
+    def __init__(self, chain, address):
+        self.chain = chain
         self.address = address
-        self.ticks_df = pd.DataFrame()
+        self.ticksState_df = pd.DataFrame()
+        self.ticksFlow_df = pd.DataFrame()
+
+
+    def __createQuery(self, type, address, graphQl_endpoints):
+
+
+        if(type=="PoolDetails"):
+            query = """
+            {
+                pool(id: \""""+address+"""\"){
+                    
+                    id
+                    
+                    token0 {
+                        id
+                        symbol
+                        name
+                        decimals
+                    }
+                    
+                    token1 {
+                        id
+                        symbol
+                        name
+                        decimals
+                    }
+                    
+                    feeTier
+                    tick
+                    totalValueLockedUSD
+                    sqrtPrice
+                    liquidity
+                    }
+                }
+            
+            """
+        
+        if(type=="TickState"):
+        
+            query = """
+                {
+            pool(id: \""""+address+"""\") {
+                    ticks (orderBy: liquidityGross, orderDirection: desc, first: """+str(graphQl_endpoints[1])+""", skip: """+str(graphQl_endpoints[2])+""", where:{liquidityNet_gt: 0} ) {
+                    tickIdx,
+                liquidityNet
+                liquidityGross
+                feeGrowthOutside0X128
+                feeGrowthOutside1X128
+                        }
+                    }
+                }
+
+            """
+
+
+        if(type=="TickFlow"):
+            query = """
+            {
+            pool(id: \""""+address+"""\"){
+	            swaps (orderBy: timestamp, orderDirection: desc, first: """+str(graphQl_endpoints[1])+""", skip: """+str(graphQl_endpoints[2])+""") {
+                tick
+                timestamp
+                amount0
+                amount1
+		            }
+                }
+            }
+            """
+        return query
+
+    def getPoolDetails(self, graphQl_endpoints):
+        
+        #get pool state from subgraph API
+        response = requests.post(graphQl_endpoints[0], json={'query': self.__createQuery("PoolDetails", self.address, graphQl_endpoints)}).json()
+        poolDetails = response['data']['pool']
+
+        return poolDetails
+
+
+    def getTicksState(self, graphQl_endpoints):
+        
+        
+        #New state dataframe
+        self.ticksState_df = pd.DataFrame()
+
+        #get ticks state from subgraph API
+        while True:
+            response  = requests.post(graphQl_endpoints[0], json={'query': self.__createQuery("TickState", self.address, graphQl_endpoints)}).json()
+            if(len(response['data']['pool']['ticks'])>0):
+                self.ticksState_df = self.ticksState_df.append(pd.json_normalize(response['data']['pool']['ticks']))
+                graphQl_endpoints[2] = graphQl_endpoints[2] + graphQl_endpoints[1]
+            else:
+                break
+        return self.ticksState_df
+
+    def getTickFlow(self, graphQl_endpoints, tick_length):
+        
+        #New tickFlow dataframe
+        self.ticksFlow_df = pd.DataFrame()
+
+        #get ticks flow from subgraph API
+        while (graphQl_endpoints[2]< tick_length):
+            response  = requests.post(graphQl_endpoints[0], json={'query': self.__createQuery("TickFlow", self.address, graphQl_endpoints)}).json()
+            if(len(response['data']['pool']['swaps'])>0):
+                self.ticksFlow_df = self.ticksFlow_df.append(pd.json_normalize(response['data']['pool']['swaps']))
+                graphQl_endpoints[2] = graphQl_endpoints[2] + graphQl_endpoints[1]
+            else:
+                break
+
+        return self.ticksFlow_df
+
+
+
+
         
